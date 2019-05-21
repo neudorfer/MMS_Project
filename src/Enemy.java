c95666b1f1cd41b7b4254f5f3346c48fbac83d13
brickflash <brickflash@192.168.0.127>
2019-05-21 20:11:45 +0200

    enemyManagement 1

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,10 +2,37 @@
 
 public class Enemy extends GameObject {
 	
-	boolean visible = ture;
+	boolean alive;
+	boolean shooting;
 
 	public Enemy(int x, int y, int width, int heigth, String imagePath, int speedx, int speedy) {
 		super(x, y, width, heigth, imagePath, speedx, speedy);
+		this.alive = true;
+		this.shooting = false;
 	}
 	
+	public Enemy getEnemy() {
+		return this;
+	}
+	
+	public boolean getShooting() {
+		return shooing;
+	}
+	
+	public boolean getAlive() {
+		return alive;
+	}
+	
+	public boolean setShooting(boolean status) {
+		shooting = status;
+	}
+	
+	public boolean setAlive() {
+		alive = false;
+	}
+	
+	public void update() {
+		x += speedx;
+		y += speedy;
+	}
 }
--- src/EnemyManagement.java
+++ src/EnemyManagement.java
@@ -4,7 +4,32 @@
 public class EnemyManagement {
 	private ArrayList<Enemy> enemys;
 	
-	public void update(Enemy enemy) {
+	public EnemyManagement() {
+		enemys = new ArrayList<Enemy>();
+		int x=1;
+		int y=0;
+		int width=0;
+		int heigth=0;
+		String imagePath = "/enemy.png";
+		int speedx=0;
+		int speedy=0;
+		Enemy newEnemy = Enemy(x, y, width, heigth, imagePath, speedx, speedy);
 		
+		for (int i = 0; i<15 ; i++) {
+			enemys.Add(newEnemy);
+		}
+	}
+	
+	public void update(Enemy enemy) {
+		//Set last shooting enemy false
+		for(Enemy enemy : a.getEnemies()) {
+			if (enemy.getShooting == true) {
+				enemy.setShooting(false);			
+			}
+		}
+		//Set new shooting enemy
+		Random rand = new Random();
+		int n = rand.nextInt(15);
+		enemys[n].setShooting(true);
 	}
 }
